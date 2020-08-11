ac54d74dd0a03557d5a26b4747658311c7c49421
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-08-11 17:06:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+function sayHiToGrandma(string) {
+  if (string.toLowerCase === lowercase) {
+    return `I can't hear you!`
+  } else if (string.toUpperCase === uppercase) {
+    return `YES INDEED!`
+  } else if (string.toLowerCase === mixedCase || string.toUpperCase === mixedCase)
+    return `I love you, too.`
+}
